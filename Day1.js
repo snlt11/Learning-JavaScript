eb68d38044fdc1959269f3b2d7efc96c30b63be3
Sai Nay Lin Thar <sainaylinthar@gmail.com>
2024-01-20 23:03:52 +0630

    Day 1

---
+++ Day1.js
@@ -0,0 +1,122 @@
+
+// let x = 12;
+// let y = "12";
+// z = x === y; 
+// // === (type, value) == (value)
+// console.log(z);
+
+// let x = 20;
+// let y = 10;
+// // x = x + 1;
+// // x += 1;
+// // x++;
+// // x--;
+// console.log(x++);
+// console.log(x);
+// console.log(++x);
+// console.log(x);
+// console.log(y);
+// console.log(++y);
+// console.log(y--);
+// console.log(y);
+
+// let x = 30;
+// let y = 20;
+// let z = x + y;
+// let a = x - y;
+// let b = x / y;
+// let c = x % y;
+// let d = x * y;
+// console.log(d);
+
+// const array = new Array(1,2,3,4,5,6,7,8,9);
+// // array.push(10,11);
+// array[1] = 10;
+// console.table(array);
+
+// const person = {
+// name: 'John',
+//     age: 34,
+// }
+// // person = 'test'; Error
+// person.name = 'test';
+// console.table(person);
+
+// let person = {
+//     name: 'John',
+// }
+// console.log(person.name);
+
+// let person = {
+//     name: 'John',
+//     age : 21,
+//     drink: true,
+//     family : ['U BA','Daw Hla','Ma Mya'],
+//     own : {
+//         car : 'Toyota',
+//         laptop : 'Asus'
+//     },
+//     doIt : function(){
+//         console.log(this.name + ' own ' + this.own.car)
+//     }
+// }
+// let json = JSON.stringify(person);
+// let obj = JSON.parse(json);
+// console.table(json);
+// console.log(obj);
+
+// let names = [
+    //     [1,2,[3,[77,22,11],8]],
+    //     [4,5,6]
+    // ];
+    
+    // console.log(names[0][2][1][2]);
+    // console.table(names);
+    
+// let test = ['one', 'two', 'three', 'four', 'five', 'six'];
+// console.table(test[1]);
+// console.log("Length " + test.length);
+// console.log(test);
+// console.table(test);
+// let FirstName = "mg mg"; //pascal case
+// let first_name = "aung aung"; //underscore conversion
+// let FirstNameSir = "mg aung"; //camel case
+// let First_Name = "aung aung"; //snake case,const variable
+
+// console.time('D');
+// let firstName = 12;
+// let lastName = "tun tun";
+// let name = firstName + " " +lastName;
+// console.log(name);
+// console.warn(name);
+// console.error(name);
+// console.log(typeof name);
+// // console.clear();
+// console.timeEnd('D');
+
+// let test;
+// if(test == undefined)
+//     console.log("bahbah");
+//     else 
+//         console.log(test);
+// let test;
+// let age = 23; // age < 21 = aung aung and age > 21 = Mg Mg
+// test = age > 21 ? "aung aung" : "Mg Mg";
+// console.log(test);
+
+// const head = document.querySelector(".head");
+// head.style.color = "red";
+// // head.style.fontSize = "100px";
+// document.querySelector(".head").style.color = "red";
+// let x = 10;
+// let y = 32;
+// let z = x + y;
+// console.log(z);
+// let x = 10; Number Type
+// let name = "mg mg"; String Type
+// console.log(x);
+// console.log(typeof(test));
+// let test = "mg mg";
+// document.querySelector("#head").innerText=test;
+// let x = 4 < 5;
+// console.log(x);
