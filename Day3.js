365443974b5930198662ba4db31a837ceaab745a
Sai Nay Lin Thar <sainaylinthar@gmail.com>
2024-01-22 15:25:32 +0630

    Day 3.0

---
+++ Day3.js
@@ -0,0 +1,95 @@
+// var num = 20;//Global Scope , redeclaredable
+// let test = 200; //Block Scope(local variable)
+
+// console.log(window.num);
+// console.log(window.test);
+
+// for(let i=0; i< 10; i++) {
+//     console.log(i);
+// }
+// console.log(i);
+// for(var i=0; i< 10; i++) {
+//     console.log(i);
+// }
+// console.log("********************************");
+// console.log(i);
+
+// function doIt(){
+//     var a = 10;
+//     let b = 20;
+//     console.log(a);
+//     console.log(b);
+// }
+// doIt();
+
+
+// let str = "This is a test string that contains a number of characters";
+// // str += " that is expected to be in the format expected  by the";
+// let add = "test string";
+// let sum = str + add;
+// console.log(str);
+// console.log(sum);
+
+//Concat
+// let str = "this is a test string";
+// let aa = "hello world is a test string";
+// str = str.concat(" ** ",aa);
+// console.log(str);
+
+// let str = "this is a test string"; 
+// test = str.toLowerCase();
+// console.log(test[3]);
+// console.log(str.lastIndexOf('t'));    
+// console.log(str.charAt(1));
+// console.log(str.length);
+//  console.log(str.substr(1,3))
+//  console.log(str.slice(0,8));
+// console.log(str.slice(-6));
+// console.log(str.slice(-10,-4));
+// console.log(str.split(" "));
+// console.log(str);
+// let bb = str.replace("test","result");
+// console.log(bb);
+// console.log(str.includes("is"));
+
+// Window Object
+// window.close();
+// window.alert("123");
+// let test = window.prompt("what is your name?");
+// document.querySelector("#head").innerHTML = test;
+
+// let con = confirm("Are you 18");
+// let test = con ? "Yes I am" : "No i am not";
+// document.querySelector("#head").innerText = test;
+
+// document.querySelector("#head").innerText = window.innerHeight;
+// document.querySelector("#head").innerText = window.innerWidth;
+
+// let test = document.querySelector("#head");
+// test.style.backgroundColor = "black";
+// test.style.height = "4000px";
+// test.style.color = "white";
+// console.log(window.scrollY);
+// window.location.href = "extra.html";
+// document.querySelector("#btn").addEventListener('click', function(){
+//     // window.location.href = "extra.html";
+//     // window.location.reload();
+//     console.log(window.location.search);
+// })
+
+// DOM
+// console.log(document.all[4]);
+// console.log(document.URL);
+// console.log(document.characterSet);
+// Form Handling
+// console.log(document.forms[2].action);
+// console.log(document.forms[1].method);
+// console.log(document.links);
+// console.log(document.links[0].innerText);
+// console.log(document.links[1].className);
+// console.log(document.images[1].currentSrc)
+// console.log(document.images[1].src);
+// console.log(document.images[1].alt);
+// console.log(document.scripts);
+// console.log(document.scripts[0].baseURI);
+
